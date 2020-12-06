6ae23dac9742b997363e1e28086696f054fe5059
Vishnu MD <vishnudivakaran1@gmail.com>
2020-12-06 19:49:08 +0530

    Update views.py
    Commenting ad function

--- travelApp/views.py
+++ travelApp/views.py
@@ -6,8 +6,8 @@ def hello(request):
     return render(request, 'index.html')
 
 
-def add(request):
-    num1 = int(request.POST['num1'])
-    num2 = int(request.POST['num2'])
-    res = num1 + num2
-    return render(request, 'hello.html', {'result': res})
+# def add(request):
+#     num1 = int(request.POST['num1'])
+#     num2 = int(request.POST['num2'])
+#     res = num1 + num2
+#     return render(request, 'hello.html', {'result': res})
